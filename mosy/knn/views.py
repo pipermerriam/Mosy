661adf564cea1dd63439c83373b308b3f0335f2e
Aaron Merriam <amerriam@fusionbox.com>
2011-08-11 15:44:26 -0600

    Fix so that basic lsh view works

--- mosy/knn/views.py
+++ mosy/knn/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.db import connection, transaction
 
-from mosy.knn.models import LSH, DataPoint, Neighbors
+from mosy.knn.models import LSH
 
 def index(request):
   template = 'index.html'
